a7572e60e7cbcd49633ca9fef619c72710f2ba65
Curtis Floth <cfdezine@gmail.com>
2019-02-18 08:05:10 -0700

    Adding javascript menu functionality

--- docs/assets/js/bundle.js
+++ docs/assets/js/bundle.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(o){console.log("hi")});
+jQuery(document).ready(function(t){t("body").on("click",".nav_main_menus_button",function(n){n.preventDefault(),t(this).toggleClass("active")})});
